91de7201b1c886c342345729a09e7ed7da7adbf3
Eva Martinez <martinez.eva.13@gmail.com>
2024-04-03 20:42:49 +0200

    pantallas busqueda y chef ( sin desarrollar)

---
+++ app/src/main/java/com/example/choosechef/Activity_chef.java
@@ -0,0 +1,16 @@
+package com.example.choosechef;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_chef extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef);
+    }
+    private void anadirservicio() {
+
+    }
+}
---
+++ app/src/main/java/com/example/choosechef/Activity_chef_ampliado.java
@@ -0,0 +1,13 @@
+package com.example.choosechef;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_chef_ampliado extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_ampliado);
+    }
+}
